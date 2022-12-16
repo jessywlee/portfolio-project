d388f42aaf2cd070877b6cc67596e5def75ad824
yewon.lee <yewon.lee@seavantage.com>
2022-12-16 18:50:42 +0900

    Working on Projects section layout

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { SocialIcon } from "react-social-icons";
 import Image from "next/image";
 
 type Props = {};
@@ -9,33 +10,32 @@ export default function Projects({}: Props) {
   return (
     <div
       className="h-screen flex flex-col relative overflow-hidden text-left md:flex-row max-w-full
-		justify-evenly mx-auto items-center z-0">
+		justify-evenly mx-auto items-center z-0"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-400 text-4xl">
-        Projects
+        Projects / Studies
       </h3>
-      <div
-        className="relative w-full flex overflow-x-scroll overflow-y-hidden 
-			snap-x snap-mandatory z-20 scrollbar-thin
-			scrollbar-track-gray-400/20 scrollbar-thumb-[#e05abd]/40">
+      <div className="w-screen grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 snap-center">
         {projects.map((project) => (
           <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.2 }}
             key={project}
-            className="w-screen h-screen snap-center flex-shrink-0 flex flex-col space-y-5 
-						items-center justify-center p-20 md:p-44">
-            <Image
-              src="https://img.icons8.com/external-flat-satawat-anukul/512/external-designthinking-design-thinking-flat-flat-satawat-anukul-35.png"
-              alt="project"
-              width="240"
-              height="240"
+            className="w-[300px] h-[300px]
+						items-center justify-center group flex flex-col relative overflow-y-scroll"
+          >
+            <SocialIcon
+              url="https://jessywlee.medium.com/"
+              target="_blank"
+              fgColor="gray"
+              bgColor="transparent"
             />
-            <div className="max-w-6xl space-y-10 px-0 md:px-10">
+            <div>
               <h4 className="text-center text-3xl font-semibold">
                 Case Study: blah blah
               </h4>
-              <p className="text-center md:text-left">
+              <p className="text-center md:text-left mt-2">
                 There was something in the tree. It was difficult to tell from
                 the ground, but Rachael could see movement. She squinted her
                 eyes and peered in the direction of the movement, trying to
