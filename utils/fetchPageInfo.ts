09604ceac75fcf8a5089dca1e3eda062be6ea6f4
yewon.lee <yewon.lee@seavantage.com>
2022-12-21 19:48:38 +0900

    Create utils for fetch

---
+++ utils/fetchPageInfo.ts
@@ -0,0 +1,11 @@
+import { PageInfo } from "../typings";
+
+export const fetchPageInfos = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+  );
+  const data = await res.json();
+  const pageInfo: PageInfo = data.pageInfo;
+
+  return pageInfo;
+};
