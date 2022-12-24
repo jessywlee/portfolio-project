a6468636c4509fc44f2add002850b4653dcfc2b9
Jessica Lee <to.jessywlee@gmail.com>
2022-12-25 00:25:23 +0900

    delete console

--- utils/Util.ts
+++ utils/Util.ts
@@ -33,7 +33,6 @@ export const fetchProjects = async () => {
 export const fetchSkills = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
   const data = await res.json();
-  console.log(data);
   const skills: Skills = data.skills;
 
   return skills;
