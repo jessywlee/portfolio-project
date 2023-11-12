c6d8a32248fa3a5f865698ba49b16a2c62b02b9b
Jessica Lee <to.jessywlee@gmail.com>
2023-11-12 22:51:14 +0900

    Style fix

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -21,12 +21,10 @@ export default function WorkExprience({ experiences }: Props) {
       }}
       className="h-screen flex flex-col relative 
 			text-center md:text-left md:flex-row max-w-7xl overflow-y-scroll
-			px-10 justify-evenly mx-auto items-center"
-    >
+			px-10 justify-evenly mx-auto items-center">
       <h3
         className="absolute top-24 text-center z-30 uppercase tracking-[20px] 
-      -mr-[20px] text-gray-400 text-3xl md:text-4xl"
-      >
+      -mr-[20px] text-gray-400 text-3xl md:text-4xl">
         Experience
       </h3>
 
@@ -40,10 +38,9 @@ export default function WorkExprience({ experiences }: Props) {
         transition={{
           duration: 1.5,
         }}
-        className="absolute top-[15%] sm:top-[20%] desktop:top-[25%] max-w-[420px] flex flex-col sm:flex-row sm:overflow-x-scroll 
+        className="absolute top-[15%] sm:top-[20%] desktop:top-[25%] max-w-[560px] flex flex-col sm:flex-row sm:overflow-x-scroll 
         sm:space-x-5 p-10 pb-[100px] sm:pb-0 sm:snap-x snap-mandatory scrollbar-thin
-        scrollbar-track-gray-400/20 scrollbar-thumb-[#e05abd]/40 z-0"
-      >
+        scrollbar-track-gray-400/20 scrollbar-thumb-[#e05abd]/40 z-0">
         {experiences
           .sort((a, b) => a.order - b.order)
           .map((experience) => (
